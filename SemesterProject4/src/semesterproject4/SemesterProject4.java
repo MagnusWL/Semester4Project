8ba748f335f3947e4a98eefa7289558f01140557
MagnusWL <magnuslarsen1@gmail.com>
2017-02-14 13:10:50 +0100

    Add files via upload

---
+++ SemesterProject4/src/semesterproject4/SemesterProject4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semesterproject4;
+
+/**
+ *
+ * @author Magnus
+ */
+public class SemesterProject4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
